7bafb0b817622ae23e47fe920fb6c08d6246ee91
Andre Goncalves <andrefcgoncalves@gmail.com>
2021-08-01 12:59:45 +0100

    Added cron to move draft tasks to ready

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const app = express();
 
 const cors = require('cors');
 
+require('./cron');
+
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
