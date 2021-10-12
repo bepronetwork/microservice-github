abfa0c906c77e9170887f359f370319dbf72d025
moshmage <moshmage@gmail.com>
2021-10-12 17:05:03 +0100

    -1 on query.page

--- middlewares/paginate.middleware.js
+++ middlewares/paginate.middleware.js
@@ -1,6 +1,6 @@
-function paginate(query = {}, {page = 1,} = {page: 1,}) {
+function paginate(query = {}, {page = 0,} = {page: 0,}) {
   const limit = 10;
-  const offset = page * limit;
+  const offset = (page - 1) * limit;
   return ({
     ...query,
     offset,
