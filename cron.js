0773ee2f7c822118d6ec38c8daf874b9fd55de77
moshmage <moshmage@gmail.com>
2021-10-26 09:44:14 +0100

    run change draft issues on start

--- cron.js
+++ cron.js
@@ -4,41 +4,44 @@ const { Op } = require('sequelize');
 const GithubService = require('./services/github.service');
 const dayjs = require('dayjs');
 
+async function changeDraftedIssues() {
+  console.log('##### Checking draft issues to move to open');
+  const issues = await models.issue.findAll(
+    {
+      where: {
+        createdAt: {
+          [Op.lt]: dayjs().subtract(3, 'day').toDate(),
+        },
+        state: 'draft',
+      },
+    });
+
+  for (const issue of issues) {
+    try {
+      const repo = await models.repositories.findOne({where: {id: issue.repository_id}})
+      await GithubService.removeDraftLabelFromIssue(issue.githubId, repo?.githubPath);
+    } catch (error) {
+      // label not exists, ignoring
+    }
+    issue.state = 'open';
+    await issue.save();
+  }
+
+  console.log('##### FINISH Checking draft issues to move to open');
+}
+
 try {
   console.log('Starting CronJob...');
   const stamp = +new Date();
   new CronJob({
     cronTime: '00 8 * * *',
-    onTick: async () => {
-      console.log('##### Checking draft issues to move to open');
-      const issues = await models.issue.findAll(
-        {
-          where: {
-            createdAt: {
-              [Op.lt]: dayjs().subtract(3, 'day').toDate(),
-            },
-            state: 'draft',
-          },
-        });
-
-      for (const issue of issues) {
-        try {
-          const repo = await models.repositories.findOne({where: {id: issue.repository_id}})
-          await GithubService.removeDraftLabelFromIssue(issue.githubId, repo?.githubPath);
-        } catch (error) {
-          // label not exists, ignoring
-        }
-        issue.state = 'open';
-        await issue.save();
-      }
-
-      console.log('##### FINISH Checking draft issues to move to open');
-    },
+    onTick: () => changeDraftedIssues(),
     start: true,
     timeZone: 'Europe/Lisbon'
   });
 
   console.log(`Started!`, +new Date() - stamp, `ms`);
+  changeDraftedIssues();
 } catch (ex) {
   console.log('cron pattern not valid');
 }
